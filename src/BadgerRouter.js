887f14c3399397bc93f7b4af15edd62396d8b41d
michaeltran5 <mttran7@wisc.edu>
2024-09-23 23:29:04 -0500

    started the home page

--- src/BadgerRouter.js
+++ src/BadgerRouter.js
@@ -3,12 +3,13 @@ import CourseSearchScreen from "./CourseSearchScreen";
 import App from "./App";
 import About from "./About";
 import Report from "./Report";
+import Home from "./Home"
 function BadgerRouter(){
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/cuck-the-curve" element={<App/>}>
-					<Route path="/cuck-the-curve/home" element={<></>}/>
+					<Route path="/cuck-the-curve/home" element={<Home/>}/>
                     <Route path="/cuck-the-curve/course_search" element={<CourseSearchScreen/>}/>
                     <Route path="/cuck-the-curve/about" element={<About/>}/>
 					<Route path="/cuck-the-curve/report" element={<Report/>}/>
